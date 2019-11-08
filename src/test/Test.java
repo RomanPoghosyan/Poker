5df71c01dfefdd75722e4868dc14ead9acab00ed
RomanPoghosyan <roman.poghos@gmail.com>
2019-11-08 23:29:05 +0400

    added classes Poker, Player, Card

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+import com.poker.Poker;
+
+public class Test {
+    public static void main(String[] args) {
+        Poker p = new Poker(2);
+        p.anyoneWantsToChange();
+    }
+}
