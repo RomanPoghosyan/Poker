b2c49557c5d140a3b75a6a0f7bb027e3f82a5795
Ruviks <poghosyan26@gmail.com>
2019-11-09 00:27:30 -0800

    Formatted text

--- src/com/poker/Player.java
+++ src/com/poker/Player.java
@@ -1,43 +1,47 @@
 package com.poker;
 
 public class Player {
-	//Fields
+	// Fields
 	private String playerName;
-	private int index=0;
-	private Card[] cards = new Card[5] ;
-	//getter setter 
-	public String getplayerName()
-	{
+	private int index = 0;
+	private Card[] cards = new Card[5];
+
+	// getter setter
+	public String getplayerName() {
 		return playerName;
 	}
-   //Constructor
-    public Player(String playerName)
-    {   this.playerName=playerName;
-    	for(int i=0;i<=5;i++)
-    	cards[i]=null;
-    }
-	//Replaces a card and returns the replaced card
-	public Card replace(Card card,Card cardtoreplaceWith)
-    {  Card tempCard =null;
-    	for(int i=0;i<=5;i++)
-    		if(cards[i].equals(card)) {
-    			tempCard=cards[i];
-    			cards[i]=cardtoreplaceWith;
-    			return tempCard;
-    		}
-    		
-    	
-    	return null;
-    }
-	//Adds a card if there is a free place otherwise returns false
-    public boolean add(Card cardtoAdd)
-    { if(index==5)return false;
-    	for(int i=0;i<=5;i++)
-    	{if(cards[i]==null)
-    		{cards[i]=cardtoAdd;
-    		index++;
-    		return true;}
-    	}
-    	return false;
-    }
+
+	// Constructor
+	public Player(String playerName) {
+		this.playerName = playerName;
+		for (int i = 0; i <= 5; i++)
+			cards[i] = null;
+	}
+
+	// Replaces a card and returns the replaced card
+	public Card replace(Card card, Card cardtoreplaceWith) {
+		Card tempCard = null;
+		for (int i = 0; i <= 5; i++)
+			if (cards[i].equals(card)) {
+				tempCard = cards[i];
+				cards[i] = cardtoreplaceWith;
+				return tempCard;
+			}
+
+		return null;
+	}
+
+	// Adds a card if there is a free place otherwise returns false
+	public boolean add(Card cardtoAdd) {
+		if (index == 5)
+			return false;
+		for (int i = 0; i <= 5; i++) {
+			if (cards[i] == null) {
+				cards[i] = cardtoAdd;
+				index++;
+				return true;
+			}
+		}
+		return false;
+	}
 }
