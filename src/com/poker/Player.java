e10cc6561930ee58d6e2e8579d3d9b67d1c8264e
Ruviks <poghosyan26@gmail.com>
2019-11-08 21:01:49 -0800

    Updated Player

--- src/com/poker/Player.java
+++ src/com/poker/Player.java
@@ -2,10 +2,15 @@ package com.poker;
 
 public class Player {
 	//Fields
+	private String playerName;
 	private int index=0;
 	private int replaceCount=0;
 	private Card[] cards = new Card[5] ;
-	//getter setter for replaceCount
+	//getter setter 
+	public String getplayerName()
+	{
+		return playerName;
+	}
 	public int getreplaceCount() { return replaceCount;}
 	public boolean setreplaceCount(int n) {
 		if(n>0 && n<=3)replaceCount=n;
@@ -16,12 +21,12 @@ public class Player {
 		
 	}
    //Constructor
-    public Player()
-    {   
+    public Player(String playerName)
+    {   this.playerName=playerName;
     	for(int i=0;i<=5;i++)
     	cards[i]=null;
     }
-	//set manipulation
+	//Replaces a card and returns the replaced card
 	public Card replace(Card card,Card cardtoreplaceWith)
     {  Card tempCard =null;
     	for(int i=0;i<=5;i++)
@@ -34,7 +39,8 @@ public class Player {
     	
     	return null;
     }
-    public boolean replace(Card cardtoAdd)
+	//Adds a card if there is a free place otherwise returns false
+    public boolean add(Card cardtoAdd)
     { if(index==5)return false;
     	for(int i=0;i<=5;i++)
     	{if(cards[i]==null)
