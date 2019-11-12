081f12cef1688688e7153a909b2895fa03cabdff
RomanPoghosyan <roman.poghos@gmail.com>
2019-11-12 21:05:46 +0400

    suit with emoji

--- src/com/poker/Ranks.java
+++ src/com/poker/Ranks.java
@@ -1,5 +1,26 @@
 package com.poker;
 
 public enum Ranks {
-    DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    DEUCE("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("J"),
+    QUEEN("Q"),
+    KING("K"),
+    ACE("A");
+
+    private String value;
+    Ranks(String value){
+        this.value = value;
+    }
+
+    public String toString(){
+        return this.value;
+    }
 }
--- src/com/poker/Suits.java
+++ src/com/poker/Suits.java
@@ -1,5 +1,13 @@
 package com.poker;
 
 public enum Suits {
-    CLUBS, DIAMONDS, HEARTS, SPADES
+    CLUBS("♣"), DIAMONDS("♦"), HEARTS("♥"), SPADES("♠");
+    private String value;
+    Suits(String value){
+        this.value = value;
+    }
+
+    public String toString(){
+        return this.value;
+    }
 }
